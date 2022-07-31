c53b82c4531913d6f0daed191a6638fec4156d8e
Aditya Painuli <adityapainuli2004@gmail.com>
2022-07-31 19:51:05 +0530

    second-commit

--- components/Post.jsx
+++ components/Post.jsx
@@ -8,8 +8,71 @@ import {
 } from "@heroicons/react/outline";
 
 import { HeartIcon as HeartIconFilled } from "@heroicons/react/solid";
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  onSnapshot,
+  orderBy,
+  query,
+  serverTimestamp,
+  setDoc,
+} from "firebase/firestore";
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+import { db } from "../firebase";
+import Momemt from "react-moment";
 
 const Post = ({ id, userImg, picture, caption, username }) => {
+  const { data: session } = useSession();
+  const [comments, setComments] = useState([]);
+  const [comment, setComment] = useState([]);
+  const [likes, setLikes] = useState([]);
+  const [hasLiked, sethasLiked] = useState(false);
+  useEffect(
+    () =>
+      onSnapshot(
+        query(
+          collection(db, "posts", id, "comments"),
+          orderBy("timestamp", "desc")
+        ),
+        (snapshot) => setComments(snapshot.docs)
+      ),
+    [db, id]
+  );
+  useEffect(
+    () =>
+      onSnapshot(collection(db, "posts", id, "likes"), (snapshot) =>
+        setLikes(snapshot.docs)
+      ),
+    [db, id]
+  );
+  useEffect(() => {
+    return sethasLiked(
+      likes.findIndex((like) => like.id === session?.user?.uid) !== -1
+    );
+  }, [likes]);
+  const likePost = async () => {
+    if (hasLiked) {
+      await deleteDoc(doc(db, "posts", id, "likes", session.user.uid));
+    } else {
+      await setDoc(doc(db, "posts", id, "likes", session.user.uid), {
+        username: session.user.username,
+      });
+    }
+  };
+  const sendComment = async (e) => {
+    e.preventDefault();
+    const commentToSend = comment;
+    setComment("");
+    await addDoc(collection(db, "posts", id, "comments"), {
+      comment: commentToSend,
+      username: session.user.username,
+      userImage: session.user.image,
+      timestamp: serverTimestamp(),
+    });
+  };
   return (
     <div className="bg-white my-7 border rounded-sm">
       <div className="flex items-center p-5">
@@ -22,27 +85,66 @@ const Post = ({ id, userImg, picture, caption, username }) => {
         <DotsHorizontalIcon className="h-5" />
       </div>
       <img src={picture} alt={caption} className="object-cover h-auto w-full" />
-      <div className="flex justify-between px-4 pt-4">
-        <div className="flex space-x-4 ">
-          <HeartIcon className="btn" />
-          <ChatIcon className="btn" />
-          <PaperAirplaneIcon className="btn" />
+      {session && (
+        <div className="flex justify-between px-4 pt-4">
+          <div className="flex space-x-4 ">
+            <HeartIcon onClick={likePost} className="btn" />
+            <ChatIcon className="btn" />
+            <PaperAirplaneIcon className="btn" />
+          </div>
+          <BookmarkIcon className="btn" />
         </div>
-        <BookmarkIcon className="btn" />
-      </div>
+      )}
       <p className="p-5 truncate">
         <span className="font-bold mr-1">{username} </span>
         {caption}
       </p>
-      <form className="flex items-center p-4">
-        <EmojiHappyIcon className="h-7 mr-2" />
-        <input
-          type="text"
-          placeholder="Add a comment..."
-          className="border-none flex-1 focus:ring-0 outline-none"
-        />
-        <button className="font-semibold text-blue-400">Post</button>
-      </form>
+      {comments.length > 0 && (
+        <div className="ml-10 h-20 overflow-y-scroll scrollbar-thumb-black scrollbar-thin">
+          {comments.map((comment) => (
+            <div
+              key={comment.id}
+              className="flex items-center space-x-2 mb-3 justify-between"
+            >
+              <div className="flex">
+                <img
+                  src={comment.data().userImage}
+                  alt="user-photo"
+                  className="h-7 rounded-full mr-2"
+                />
+                <p>
+                  <span className="font-semibold">
+                    {comment.data().username}{" "}
+                  </span>
+                  {comment.data().comment}
+                </p>
+              </div>
+              <Momemt fromNow className="pr-5 text-xs">
+                {comment.data().timestamp?.toDate()}
+              </Momemt>
+            </div>
+          ))}
+        </div>
+      )}
+      {session && (
+        <form className="flex items-center p-4">
+          <EmojiHappyIcon className="h-7 mr-2" />
+          <input
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
+            type="text"
+            placeholder="Add a comment..."
+            className="border-none flex-1 focus:ring-0 outline-none"
+          />
+          <button
+            disabled={!comment}
+            onClick={(e) => sendComment(e)}
+            className="font-semibold text-blue-400"
+          >
+            Post
+          </button>
+        </form>
+      )}
     </div>
   );
 };
--- components/Posts.jsx
+++ components/Posts.jsx
@@ -1,55 +1,28 @@
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { db } from "../firebase";
 import Post from "./Post";
 
-const posts = [
-  {
-    id: "123",
-    username: "naruto_uzumaki.23",
-    userImg:
-      "https://i.pinimg.com/originals/c0/ff/5b/c0ff5be1deb59a06e0d4a4c303e986cf.jpg",
-    picture:
-      "https://mernetwork.com/index/attachment.php?attachmentid=32096&d=1439171621",
-    caption: "Last night bumped sakura bitch😈😈",
-  },
-  {
-    id: "123",
-    username: "naruto_uzumaki.23",
-    userImg:
-      "https://i.pinimg.com/originals/c0/ff/5b/c0ff5be1deb59a06e0d4a4c303e986cf.jpg",
-    picture:
-      "http://images1.fanpop.com/images/image_uploads/Naruto-uzumaki-naruto-987853_730_548.jpg",
-    caption: "Last night bumped sakura bitch😈😈",
-  },
-  {
-    id: "123",
-    username: "naruto_uzumaki.23",
-    userImg:
-      "https://i.pinimg.com/originals/c0/ff/5b/c0ff5be1deb59a06e0d4a4c303e986cf.jpg",
-    picture:
-      "http://images1.fanpop.com/images/image_uploads/Naruto-uzumaki-naruto-987853_730_548.jpg",
-    caption: "Last night bumped sakura bitch😈😈",
-  },
-  {
-    id: "123",
-    username: "naruto_uzumaki.23",
-    userImg:
-      "https://i.pinimg.com/originals/c0/ff/5b/c0ff5be1deb59a06e0d4a4c303e986cf.jpg",
-    picture:
-      "http://images1.fanpop.com/images/image_uploads/Naruto-uzumaki-naruto-987853_730_548.jpg",
-    caption: "Last night bumped sakura bitch😈😈",
-  },
-];
-
 const Posts = () => {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    return onSnapshot(
+      query(collection(db, "posts"), orderBy("timestamp", "desc")),
+      (snapshot) => {
+        setPosts(snapshot.docs);
+      }
+    );
+  }, [db]);
   return (
     <div>
       {posts.map((post) => (
         <Post
           key={post.id}
           id={post.id}
-          username={post.username}
-          userImg={post.userImg}
-          picture={post.picture}
-          caption={post.caption}
+          username={post.data().username}
+          userImg={post.data().profileImg}
+          picture={post.data().image}
+          caption={post.data().caption}
         />
       ))}
     </div>
